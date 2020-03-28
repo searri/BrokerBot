892bf34bf8a555d8b26db4e5e62241d2329a7d2f
Rick Sear <searri98@gmail.com>
2020-03-27 20:39:43 -0500

    Make a stock struct

---
+++ MasterBoard/Stocks.h
@@ -0,0 +1,11 @@
+struct stock
+{
+    short stockName;
+    short quantity;
+    short startVal;
+    short currVal;
+    short amtChange;
+
+    // This is the dividend per 10 stocks
+    short dividend;
+};
