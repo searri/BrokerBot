ab74e202d3271a890ae13c9faace5c0d63f051a5
Rick Sear <searri98@gmail.com>
2020-04-03 18:17:51 -0500

    Finish state 7

--- MasterBoard/MainStatesII.ino
+++ MasterBoard/MainStatesII.ino
@@ -1,21 +1,33 @@
 // Update prices in portfolio, write changes to other board
 void handleStateSeven() {
-  for(int i=0; i<NUM_STOCKS; i++) {
-    portfolio[i].currVal += portfolio[i].amtChange;
-  }
-
-  byte toSend[NUM_ITEMS];
-  for(int i=0; i<NUM_ITEMS; i++) {
-    if(portfolio[i+1].amtChange < 0) {
-      toSend[i] = portfolio[i+1].amtChange*-1;
-      toSend[i+(NUM_ITEMS/2)] = 1;
-    } else {
-      toSend[i] = portfolio[i+1].amtChange;
-      toSend[i+(NUM_ITEMS/2)] = 0;
+  if(!proceed) {
+    for(int i=0; i<NUM_STOCKS; i++) {
+      portfolio[i].currVal += portfolio[i].amtChange;
     }
+  
+    byte toSend[NUM_ITEMS];
+    for(int i=0; i<NUM_ITEMS; i++) {
+      if(portfolio[i+1].amtChange < 0) {
+        toSend[i] = portfolio[i+1].amtChange*-1;
+        toSend[i+(NUM_ITEMS/2)] = 1;
+      } else {
+        toSend[i] = portfolio[i+1].amtChange;
+        toSend[i+(NUM_ITEMS/2)] = 0;
+      }
+    }
+  
+    lcd.clear();
+    lcd.print("Waiting for");
+    lcd.setCursor(0, 1);
+    lcd.print("game server...");
+    proceed = true;
   }
 
   // TODO: write toSend to the other board
-
-  state++;
+  // TEMPORARY: will write to other board and wait for confirmation
+  if(Serial.available()) {
+    short a = Serial.parseInt();
+    state++;
+    proceed = false;
+  }
 }
